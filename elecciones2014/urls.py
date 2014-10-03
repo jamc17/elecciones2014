4d2e87c4c39c346cbcf9f5559d8949480b7876eb
jamc17 <jamcc17@gmail.com>
2014-10-02 20:18:41 -0500

    Configs statics 3

--- elecciones2014/urls.py
+++ elecciones2014/urls.py
@@ -15,3 +15,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
+)
